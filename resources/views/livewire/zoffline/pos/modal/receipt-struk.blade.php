c15ddd46d240a665f12b45d7adadf47a236ec00d
Gazali-Rahman <ghazalirahman203@gmail.com>
2026-05-31 23:18:11 +0800

    ukuran print dan struk

--- resources/views/livewire/zoffline/pos/modal/receipt-struk.blade.php
+++ resources/views/livewire/zoffline/pos/modal/receipt-struk.blade.php
@@ -6,7 +6,7 @@
            <div class="bg-white rounded-2xl shadow-xl w-full max-w-md overflow-hidden">
                <div class="p-4 bg-gray-50 border-b border-gray-100 flex justify-between items-center">
                    <h3 class="font-black text-gray-900">Struk Transaksi</h3>
-                   <div class="flex items-center gap-4">
+                   <div class="flex items-center gap-6">
                        {{-- <button
                            onclick="document.getElementById('receipt-content').classList.remove('hidden'); window.print();"
                            class="group relative text-[#1c69d4] hover:text-blue-700 font-bold text-sm flex items-center gap-1">
@@ -56,7 +56,7 @@
                        <button wire:click="getEscposBase64" wire:loading.attr="disabled"
                            class="group relative text-blue-500 hover:text-blue-700 font-bold text-sm flex items-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed">
 
-                           <svg wire:loading.remove wire:target="getEscposBase64" class="w-5 h-auto" fill="none"
+                           <svg wire:loading.remove wire:target="getEscposBase64" class="w-7 h-auto" fill="none"
                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                <path stroke-linecap="round" stroke-linejoin="round"
                                    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
@@ -84,7 +84,7 @@
                                class="group relative text-emerald-600 hover:text-emerald-700 font-bold text-xs flex items-center gap-1 transition disabled:opacity-50 disabled:cursor-not-allowed">
 
                                {{-- Icon WhatsApp (Akan hilang saat loading) --}}
-                               <svg wire:loading.remove wire:target="sendReceiptToQontak" class="w-5 h-auto"
+                               <svg wire:loading.remove wire:target="sendReceiptToQontak" class="w-7 h-auto"
                                    fill="currentColor" viewBox="0 0 24 24">
                                    <path
                                        d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946C.06 5.348 5.397 0 11.983 0c3.192.001 6.192 1.242 8.447 3.498c2.256 2.255 3.497 5.255 3.497 8.447c-.004 6.585-5.342 11.93-11.93 11.93c-2.002-.001-3.973-.503-5.729-1.457L0 24zm6.59-4.846c1.6.95 3.188 1.449 4.825 1.451c5.436 0 9.86-4.42 9.864-9.858c.002-2.634-1.023-5.11-2.887-6.974c-1.864-1.864-4.341-2.887-6.973-2.889c-5.44 0-9.865 4.42-9.869 9.859c-.001 1.706.469 3.372 1.36 4.866l-.993 3.626l3.71-.973zm11.233-6.17c-.3-.149-1.774-.875-2.046-.974c-.272-.1-.471-.149-.669.149c-.198.299-.768.974-.941 1.173c-.173.199-.347.224-.647.075c-.3-.15-1.266-.466-2.41-1.487c-.89-.794-1.49-1.774-1.664-2.073c-.173-.3-.018-.462.13-.61c.134-.133.298-.348.446-.521c.15-.173.199-.298.298-.497c.099-.198.05-.372-.025-.521c-.075-.149-.669-1.612-.916-2.207c-.242-.579-.487-.501-.669-.51l-.57-.01c-.199 0-.52.074-.792.372c-.272.297-1.04 1.016-1.04 2.479c0 1.462 1.065 2.875 1.213 3.074c.149.198 2.095 3.2 5.076 4.487c.709.306 1.263.489 1.694.626c.712.226 1.36.194 1.872.118c.571-.085 1.774-.726 2.022-1.392c.247-.667.247-1.241.173-1.392c-.074-.15-.272-.249-.571-.398z" />
@@ -127,7 +127,7 @@
                                class="group relative text-blue-600 hover:text-blue-700 font-bold text-xs flex items-center gap-1 transition disabled:opacity-50 disabled:cursor-not-allowed">
 
                                {{-- Icon Email (Akan hilang saat loading) --}}
-                               <svg wire:loading.remove wire:target="sendReceiptToEmail" class="w-5 h-auto"
+                               <svg wire:loading.remove wire:target="sendReceiptToEmail" class="w-7 h-auto"
                                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                    <path stroke-linecap="round" stroke-linejoin="round"
                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -175,7 +175,7 @@
                </div>
 
                {{-- Receipt Preview --}}
-               <div id="receipt-content" class="p-5 font-mono text-xs leading-relaxed">
+               <div id="receipt-content" class="p-5 font-mono text-xs leading-relaxed overflow-y-auto h-125">
                    <div class="text-center mb-3">
                        <p class="font-bold text-sm">SYIHAB STORE</p>
                        <p class="text-[10px] text-gray-500">
