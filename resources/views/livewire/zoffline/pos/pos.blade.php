0f0a41f5b42464a8440335abb06edbad808aa610
Gazali-Rahman <ghazalirahman203@gmail.com>
2026-06-01 15:39:59 +0800

    memperbaiki grid phone

--- resources/views/livewire/zoffline/pos/pos.blade.php
+++ resources/views/livewire/zoffline/pos/pos.blade.php
@@ -54,7 +54,7 @@
                 @if (strlen($search) >= 2)
                     @php $results = $this->searchResults; @endphp
                     @if ($results->count() > 0)
-                        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
                             @foreach ($results as $product)
                                 <button
                                     wire:click="openVariantPicker({{ $product->id }}, {{ $product->is_second_catalog ? 'true' : 'false' }})"
